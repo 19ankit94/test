ff7b8cf27fd930a4d2e7440e5ec92de35a8814b7
19ankit94 <19ankit94@gmail.com>
2020-03-13 12:29:11 +0530

    a small change

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const http = require('http');
 const PORT = 80;
 const app = (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.write("hello world");
+  res.write("we can do this :)");
   res.end();
 };
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log("server is listening on port ${PORT}");
+  console.log(`server is listening on port ${PORT}`);
 });
